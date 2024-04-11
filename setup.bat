0fe98b9b11245948074a4f46e055fa53f77ccf2f
kemilly <kemillyvictoriapereirarocha@gmail.com>
2024-04-11 11:32:39 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name "kemilly"
+git config --global user.email "kemillyvictoriapereirarocha@gmail.com"
+
+cd C:\Users\KEMILLYVICTORIAPEREI\
+
+git clone https://github.com/kemillyvictoria3/caesar-cither.git
+cd caesar-cither
+
+code .
